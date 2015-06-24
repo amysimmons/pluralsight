8acc8ff64e99e26f16cc9e68133e43ecc723a985
amysimmons <amy.simmons@ansarada.com>
2015-06-24 18:17:33 +1000

    C# fundamentals pluralsight course intro

---
+++ Sleep/Sleep/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sleep
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Please enter your name");
+			string name = Console.ReadLine();
+
+			Console.WriteLine("How many hours of sleep did you get last night?");
+			int hoursOfSleep = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("Hello, " + name);
+
+			if (hoursOfSleep < 8)
+			{
+				Console.WriteLine("You must be tired, " + name);
+			}
+			else
+			{
+				Console.WriteLine("You seem well rested, " + name);
+			}
+		}
+	}
+}
