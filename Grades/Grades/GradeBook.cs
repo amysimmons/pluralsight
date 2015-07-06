6b0a86ae5c89b5b46fc49d3329d0d9812a9f59bf
amysimmons <amy.simmons@ansarada.com>
2015-07-06 18:38:19 +1000

    grades project started

---
+++ Grades/Grades/GradeBook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+	class GradeBook
+	{
+		//adds a member that will allow me to add a grade to the gradebook
+		public void AddGrade(float grade)
+		{
+			//adds to the grades collection the grade that is passed into the method 
+			grades.Add(grade);
+		}
+		//this is a class member that represents state 
+		//it creates a list of floating point numbers
+		//this line declares a field in my program 
+		List<float> grades = new List<float>();
+	}
+}
---
+++ Grades/Grades/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//I create a variable called book with a type of Gradebook here because from inside of this Program 
+			//class I can use any other class that I have defined in this same project,
+			//as well as types from the framework class library
+			//new is a constructor method to create a new object, which is an instance of the class
+			GradeBook book = new GradeBook();
+
+			//AddGrade is available to be called on book because I have it defined as a member/method of my class 
+			book.AddGrade(91f);
+			book.AddGrade(89.5f);
+		}
+	}
+}
