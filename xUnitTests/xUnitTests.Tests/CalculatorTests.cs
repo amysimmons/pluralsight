920e13db99fcd06c495ad970d5b4d69a932e94f9
amysimmons <amy.simmons@ansarada.com>
2015-07-15 14:38:12 +1000

    xunit testing

---
+++ xUnitTests/xUnitTests.Tests/CalculatorTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Common;
+using Xunit;
+
+namespace xUnitTests.Tests
+{
+	public class CalculatorTests
+	{
+		[Fact]
+		public void ShouldAdd()
+		{
+			//Arrange 
+			//sut stands for system under test
+			var sut = new Calculator();
+
+			//Act 
+			//The act phase exercises the thing that we are testing
+			var result = sut.Add(5, 5);
+
+			//Assert 
+			//In the assert phase we check that the activites we performed in the act phase result in the expected outcome
+			Assert.Equal(10, result);
+		}
+	}
+}
---
+++ xUnitTests/xUnitTests/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnitTests
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
