5e96a64ed55b0e080da0f1f1865cde4a28468749
amysimmons <amy.simmons@ansarada.com>
2015-09-14 09:02:45 +1000

    oop fundamentals - customer mgmt system

---
+++ CustomerManagementSystem/CustomerManagementSystem.BL/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManagementSystem.BL
+{
+	public class Customer
+	{
+		//auto properties
+		public int CustomerId { get; private set; }
+
+		public string LastName { get; set; }
+		public string FirstName { get; set; }
+		public string EmailAddress { get; set; }
+		public string EHomeAddress { get; set; }
+		public string WorkAddress { get; set; }
+
+		public string FullName
+		{
+			get { return LastName + ", " + FirstName; }
+		}
+	}
+
+
+}
+
+
---
+++ CustomerManagementSystem/CustomerManagementSystem.BLTest/CustomerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CustomerManagementSystem.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomerManagementSystem.BLTest
+{
+	[TestClass]
+	public class CustomerTest
+	{
+		[TestMethod]
+		public void FullNameTestValid()
+		{
+			//arrange
+			Customer customer = new Customer();
+			customer.FirstName = "Amy";
+			customer.LastName = "Simmons";
+
+			string expected = "Simmons, Amy";
+
+			//act
+			string actual = customer.FullName;
+
+			//assert
+			Assert.AreEqual(expected, actual);
+		}
+
+	}
+}
